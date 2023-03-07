cac611513561ed73c29c39852795450fc9213974
ShalomDauphin <shalomdauphin8@gmail.com>
2023-03-07 09:47:33 +0200

    ft: debugging and finalizing user crud

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,91 +1,201 @@
-const User = require('../models/userModel');
+const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
-const { generateToken } = require('../config/jwtToken');
+const { generateToken } = require("../config/jwtToken");
+const validateMongoDbId = require("../utils/validateMongoDbId");
+const { generateRefreshToken } = require("../config/refreshtoken");
+const jwt = require("jsonwebtoken");
+// create a user
 const createUser = asyncHandler(async (req, res) => {
-    const email = req.body.email;
-    const findUser = await User.findOne({ email });
+  const email = req.body.email;
+  const findUser = await User.findOne({ email });
 
-    if (!findUser) {
-        //create a new user
-        const newUser = await User.create(req.body);
-        res.json(newUser)
-    } else {
-        //User already registered
-throw new Error('User already Exists')
-    }
-})
-
+  if (!findUser) {
+    //create a new user
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } else {
+    //User already registered
+    throw new Error("User already Exists");
+  }
+});
+// login a user
 const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
-    const findUser = await User.findOne({ email });
-    if (findUser && await findUser.isPasswordMatched(password)) {
-        res.json({
-            _id: findUser?._id,
-            firstname: findUser?.firstname,
-            lastname: findUser?.lastname,
-            email: findUser?.email,
-            mobile: findUser.mobile,
-            token: generateToken(findUser?._id)
-        }
-        )
-    } else {
-        throw new Error("Invalid Credentials!")
-    }
-    console.log(email, password)
-})
+  const { email, password } = req.body;
+  const findUser = await User.findOne({ email });
+  if (findUser && (await findUser.isPasswordMatched(password))) {
+    const refreshToken = await generateRefreshToken(findUser?._id);
+    const updateuser = await User.findOneAndUpdate(
+      findUser.id,
+      { refreshToken: refreshToken },
+      { new: true }
+    );
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      maxAge: 72 * 60 * 60 * 1000,
+    });
+    res.json({
+      _id: findUser?._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.lastname,
+      email: findUser?.email,
+      mobile: findUser.mobile,
+      token: generateToken(findUser?._id),
+    });
+  } else {
+    throw new Error("Invalid Credentials!");
+  }
+  console.log(email, password);
+});
 
 // get all users
 const getallUser = asyncHandler(async (req, res) => {
-    try {
-        const getUsers = await User.find();
-        res.json(getUsers)
-    } catch (error) {
-        throw new Error(error)
-    }
-})
+  try {
+    const getUsers = await User.find();
+    res.json(getUsers);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
+// get a single user
 const getoneUser = asyncHandler(async (req, res) => {
-    const {id} = req.params
-    try {
-        const getaUser = await User.findById(id )
-        
-        res.json(getaUser)
-    } catch (error) {
-        throw new Error(error)
-    }
-})
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const getaUser = await User.findById(id);
 
+    res.json(getaUser);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//delete a User
 const deleteaUser = asyncHandler(async (req, res) => {
-    const {id} = req.params
-    try {
-        const deleteaUser = await User.findByIdAndDelete(id  )
-            res.json({
-                msg:"This User has been Deleted Successfully!",
-                data: deleteaUser,
-        })
-    } catch (error) {
-        throw new Error(error)
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deleteaUser = await User.findByIdAndDelete(id);
+    res.json({
+      msg: "This User has been Deleted Successfully!",
+      data: deleteaUser,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+// handle refresh token
+const handleRefreshToken = asyncHandler(async (req, res) => {
+  const cookie = req.cookies;
+  if (!cookie?.refreshToken) throw new Error("No refesh token in cookies");
+  const refreshToken = cookie.refreshToken;
+  const user = await User.findOne({ refreshToken });
+  if (!user) throw new error("No refresh token found in db or not matched");
+  jwt.verify(refreshToken, process.env.JWT_SECRET, (err, decoded) => {
+    if (err || user.id !== decoded.id) {
+      throw new Error("There is something wrong with refresh token");
     }
-})
 
+    const accessToken = generateRefreshToken(user?._id);
+    res.json({ accessToken });
+  });
+});
+
+//log out functionality
+
+const logout = asyncHandler(async (req, res) => {
+  const cookie = req.cookies;
+  if (!cookie?.refreshToken) throw new Error("No refresh token in cookies");
+  const refreshToken = cookie.refreshToken;
+  const user = await User.findOne({ refreshToken });
+  if (!user) {
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: true,
+    });
+    return res.status(204);
+  }
+  await User.findOneAndUpdate(refreshToken, {
+    refreshToken: "",
+  });
+  return res.sendStatus(204);
+});
+
+//Update User
 const updateaUser = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    try {
-        const updateaUser = await User.findByIdAndUpdate(
-            id,
-            {
-            firstname: req?.body?.firstname,
-            lastname: req?.body?.lastname,
-            email: req?.body?.email,
-            mobile: req?.body?.mobile,
-        },
-            {
-                success: true
-            }
-        )
-        res.json(updateaUser)
-    } catch (error) {
-        throw new Error(error)
-    }
-})
-module.exports ={createUser, loginUser, getallUser,getoneUser,deleteaUser,updateaUser}
+  const { _id } = req.user;
+  validateMongoDbId(_id);
+  try {
+    const updateaUser = await User.findByIdAndUpdate(
+      _id,
+      {
+        firstname: req?.body?.firstname,
+        lastname: req?.body?.lastname,
+        email: req?.body?.email,
+        mobile: req?.body?.mobile,
+      },
+      {
+        success: true,
+      }
+    );
+    res.json(updateaUser);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// block a user
+const blockUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const block = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: true,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({
+      message: "User Blocked",
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//unblock a user
+const unblockUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const unblock = await User.findByIdAndUpdate(
+      id,
+      {
+        isBlocked: false,
+      },
+      {
+        new: true,
+      }
+    );
+    res.json({
+      message: "User UnBlocked",
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+module.exports = {
+  createUser,
+  loginUser,
+  getallUser,
+  getoneUser,
+  deleteaUser,
+  updateaUser,
+  blockUser,
+  unblockUser,
+  handleRefreshToken,
+  logout,
+};
--- index.js
+++ index.js
@@ -4,19 +4,18 @@ const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 const colors = require("colors");
 const dbConn = require("./config/dbConnect");
-const authRouter = require('./routes/authRoute');
+const authRouter = require("./routes/authRoute");
 const bodyParser = require("body-parser");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 //connect to db
 dbConn();
-app.use(bodyParser.json())
-app.use('/api/user', authRouter)
-
-
-
+app.use(bodyParser.json());
+app.use("/api/user", authRouter);
 app.use(notFound);
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Server is Running at PORT ${PORT}`.brightMagenta);
+  console.log(`Server is Running at PORT ${PORT}`.magenta);
 });
